7dba799c1b7844542783ef654652f16fafbde80e
Thegamer1999 <jnrnambo@gmail.com>
2023-05-04 16:31:11 +0100

    ajout du texte a l'acceuil et de la page de transaction(entrer du numero de telephone et paiement airtel money)

--- lib/Accueil/accueil.dart
+++ lib/Accueil/accueil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forqua/transaction/transaction.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
@@ -34,6 +35,7 @@ class _HomeState extends State<Home> {
                 decoration: const BoxDecoration(
                   color: Color.fromARGB(255, 226, 15, 0),
                 ),
+                child: Center(child: Text("Le jeu Forqua est conçu pour tester vos connaissances en informatique à travers une série de questions à choix multiple. Pour jouer, il vous suffit de répondre correctement à un certain nombre de questions dans un temps imparti. Si vous réussissez à répondre correctement à toutes les questions, vous aurez la chance de gagner une formation gratuite en informatique.La formation gratuite offerte par Forqua est conçue pour vous aider à améliorer vos compétences en informatique, que vous soyez débutant ou que vous ayez déjà une certaine expérience. Elle est dispensée par des experts en informatique qui sont passionnés par leur domaine et qui ont une expérience pratique dans le domaine.Pour participer au jeu Forqua, vous devrez vous inscrire en ligne sur notre site web. Une fois inscrit, vous aurez accès à la plateforme de jeu où vous pourrez commencer à répondre aux questions. Vous pouvez jouer autant de fois que vous le souhaitez pour améliorer vos résultats et augmenter vos chances de gagner la formation gratuite.Nous sommes convaincus que le jeu de quizz Forqua vous offrira une expérience amusante et éducative tout en vous aidant à améliorer vos compétences en informatique. Inscrivez-vous dès maintenant et commencez à jouer pour avoir la chance de gagner une formation gratuite en informatique",style: TextStyle(fontSize: 20,color: Colors.white),)),
               ),
             ),
             const SizedBox(
@@ -50,7 +52,12 @@ class _HomeState extends State<Home> {
                           color: const Color.fromARGB(255, 0, 0, 0),
                           fontWeight: FontWeight.bold)),
                   TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => transaction()),
+                        );
+                      },
                       child: const Text(
                         "cliquez ici.",
                         style: TextStyle(
---
+++ lib/transaction/transaction.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class transaction extends StatefulWidget {
+  const transaction({Key? key}) : super(key: key);
+
+  @override
+  State<transaction> createState() => _transactionState();
+}
+
+class _transactionState extends State<transaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Entrer votre numero Airtel-money"),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(color: Colors.grey),
+                      ),
+                      child: TextFormField(
+                        // les autres propriétés de TextFormField peuvent être ajoutées ici
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.call,color: Colors.red,),
+                          contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+
+                          labelStyle: TextStyle(color: Colors.white)
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: 200.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          //Emplacement de l'API de paiement
+                        },
+                        child: Text('Valider'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
